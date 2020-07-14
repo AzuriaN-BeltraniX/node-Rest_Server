2c4647809df2fb9e88f29244bf4207b121e2d6e3
AzuriaN BeltraniX <jibv3729@outlook.com>
2020-07-14 14:59:43 -0500

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,27 @@
 // Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Fecha de Expiración del Token (60seg, 60min, 1hr, 30d)
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// Semilla de Autenticación
+process.env.SEED = 'este-es-el-seed-desarrollo';
+
+// Base de Datos
+let urlDB; 
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    // urlDB = 'mongodb://cafe-user:cafe123@cluster0-shard-00-02.v2wru.mongodb.net/cafe';
+// urlDB = 'mongodb+srv://cafe-user:cafe123@cluster0.v2wru.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
+
+
+// mongodb://localhost:27017/cafe
+// mongodb://azbel:<xF947zFRDJkhcLc0>@cluster0.v2wru.mongodb.net/cafe
--- server/server.js
+++ server/server.js
@@ -1,47 +1,26 @@
 const express = require('express');
 const app = express();
 require('./config/config');
+const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
-app.use(bodyParser.json())
- 
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario')
+app.use(bodyParser.json());
+
+// Configuración Global de rutas 
+app.use(require('./routes/index'));
+
+mongoose.connect(process.env.URLDB, 
+    {useNewUrlParser: true, useCreateIndex: true}, 
+    (err, res) => {
+    if (err) throw err;
+
+    console.log('Base de Datos ONLINE');
+    
 });
-
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario')
-  });
 
 app.listen(process.env.PORT, () => {
     console.log('Esuchando el puerto: ', process.env.PORT);
